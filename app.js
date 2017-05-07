11645f34ab0f13c282280f4314d75a87a0f77d51
Kyle Gracey <kyle@kylegracey.com>
2017-05-07 09:13:33 -0500

    getCreateDate working
    Needed more of a switch to work. Need to look into why the for loop was
    creating an infinite loop.

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const fs = require('fs')
 // const mdConvert = require('./modules/md-converter.js')
 
 // MODULES
-const findValues = require('./modules/find-values')
+// const findValues = require('./modules/find-values')
 const trimExtension = require('./modules/trim-extension')
 const getSetting = require('./modules/get-setting')
 const getDescription = require('./modules/get-description')
+const getCreateDate = require('./modules/get-create-date')
 
 // const inputPath = process.argv[2]
 const inputPath = './files/targetexport.json'
@@ -16,14 +17,14 @@ let jsonOutput = []
 
 const parseMD = function(data) {
   // Loop through each object in Data
-  for (i=0; i<2; i++){
+  for (i=0; i<data.length; i++){
     // Get the object
     let obj = data[i]
     let newObj = {
       "Asset Name" : trimExtension(obj),
       "Asset Description" : getDescription(obj),
       BrandSubbrand : getSetting("BrandSubBrand"),
-      Created : "",
+      Created : getCreateDate(obj),
       Copyright : "",
       Tags : "",
       "Path to Assets" : obj.SourceFile,
---
+++ modules/get-create-date.js
@@ -0,0 +1,61 @@
+// Take date, remove time, reformat if needed and return proper date.
+function baseFormat(date) {
+  // Format: 2014:11:19 18:56:12-05:00
+  // Format: 2014:11:19
+  let rawDate = date.substring(0, 10);
+  let fDate = rawDate.split(':').join('-');
+  return fDate;
+}
+function formatTwo(date) {
+  // Format: 4/14/15
+  let rawDateArr = date.split('/')
+  let year = rawDateArr[2]
+  if (year.length == 2) {
+    year = "20" + year
+  }
+
+  let month = rawDateArr[0]
+  if (month.length < 2) {
+    month = "0" + month
+  }
+
+  let day = rawDateArr[1]
+  if (day.length < 2) {
+    day = "0" + day
+  }
+  const sortDateArr = [year, month, day]
+  return sortDateArr.join('-')
+}
+
+function getCreateDate(obj) {
+  const dateHeaders = ["CreateDate", "CreatedDate", "DateCreated", "DateTimeCreated", "DateTimeOriginal"]
+
+  if (obj["CreateDate"]) {
+    let formattedDate = baseFormat(obj["CreateDate"])
+    return formattedDate;
+  } else if (obj["DateCreated"]) {
+    let formattedDate = baseFormat(obj["DateCreated"])
+    return formattedDate;
+  } else if (obj["DateTimeCreated"]) {
+    let formattedDate = baseFormat(obj["DateTimeCreated"])
+    return formattedDate;
+  } else if (obj["DateTimeOriginal"]) {
+    let formattedDate = baseFormat(obj["DateTimeOriginal"])
+    return formattedDate;
+  } else if (obj["CreatedDate"]) {
+    let formattedDate = formatTwo(obj["CreatedDate"])
+    return formattedDate;
+  }
+
+}
+
+// // Test Log
+// console.log(getCreateDate({
+//     CreatedDateTime: "2012:04:02",
+//     CreationDate: "2014:11:13 16:45:13-05:00",
+//     // CreateDate: "2016:09:29 16:45:13-05:00",
+//     // CreatedDate: "4/1/15",
+//     DateCreated: "1989:07:13 16:45:13-05:00"
+//   })
+// )
+module.exports = getCreateDate
