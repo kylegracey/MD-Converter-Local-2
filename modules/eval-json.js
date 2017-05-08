b58751a6403d908381bf7d141486826a990d8894
Kyle Gracey <kyle@kylegracey.com>
2017-05-08 10:01:19 -0500

    Splitting Minor and Major Errors

--- modules/eval-json.js
+++ modules/eval-json.js
@@ -5,7 +5,23 @@ let CritErrorCount = 0
 let NoDateCount = 0
 let NoTagsCount = 0
 
-const writeLog = function(logData) {
+const tagCats = [
+  "Tags",
+  "campaign",
+  "productgroup",
+  "product",
+  "productsize",
+  "productsubtype",
+  "productgender",
+  "numberofpeople",
+  "person",
+  "teammarks",
+  "gender",
+  "shottype",
+  "sport"
+]
+
+const writeLog = function(logData, fname) {
 
   // Convert jsonOutput back to CSV
   const jsonToCsvOptions = {
@@ -14,38 +30,46 @@ const writeLog = function(logData) {
   }
   const csvOutput = csvjson.toCSV(logData, jsonToCsvOptions);
 
-  fs.writeFile("./files/ErrorLog.csv", csvOutput, function (err) {
+  fs.writeFile(`./files/${fname}.csv`, csvOutput, function (err) {
     if (err) return console.log(err);
-    console.log(`
-      ========== WARNING ==========
-      ${CritErrorCount} Critical Errors
-      ${NoDateCount} files with date warnings.
-      See Error log for details.
-      `);
-
   })
 }
 
 const evalJSON = function(jsonInput) {
   let errObjects = []
+  let critErrObjects = []
 
   jsonInput.forEach(function(obj) {
+    // console.log(`Checking ${obj["Asset Name"]}`)
+    let errObject = false
+    let critErrObject = false
+    let hasTag = false
 
     if (obj.Created == "") {
       obj["No Date"] = "!!!DATE MISSING!!!"
-      CritErrorCount
+      CritErrorCount++
+      critErrObject = true
     } else if (obj.Created == "2012-01-01") {
       obj["No Date"] = "Set to Default Date: 2012-01-01"
       NoDateCount++
+      errObject = true
     }
 
-    if (obj.Tags == "") {
+    tagCats.forEach(function(cat) {
+      if (obj[cat] !== "" && obj[cat] !== undefined) {
+        hasTag = true
+      }
+    })
+
+    if (!hasTag) {
       obj["No Tags"] = "!!!No Tags Found!!!"
       CritErrorCount++
+      critErrObject = true
       NoTagsCount++
     }
-    
-    errObjects.push(obj)
+
+    if (errObject) { errObjects.push(obj) }
+    if (critErrObject) { critErrObjects.push(obj) }
 
   })
 
@@ -61,7 +85,27 @@ const evalJSON = function(jsonInput) {
     errObjectsFormatted.push(newErrObj)
   })
 
-  writeLog(errObjectsFormatted)
+  let critErrObjectsFormatted = []
+  critErrObjects.forEach(function(errObj){
+    let newErrObj = {
+      "Asset Name": errObj["Asset Name"],
+      "Date": errObj["No Date"],
+      "Tags": errObj["No Tags"],
+      "Path to Assets": errObj["Path to Assets"]
+    }
+    critErrObjectsFormatted.push(newErrObj)
+  })
+
+  console.log(`
+    ========== WARNING ==========
+    ${CritErrorCount} Critical Errors
+    ${NoTagsCount} files with missing all tags.
+    ${NoDateCount} files with date warnings.
+    See Error log for details.
+    `);
+
+  writeLog(errObjectsFormatted, "_MinorErrorLog")
+  writeLog(critErrObjectsFormatted, "_CriticalErrorLog")
 
 }
 
